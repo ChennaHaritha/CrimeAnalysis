727c765af9d53a703af6bbd2bb73adcedee6cfc1
Chinni Pushpa Sowjanya <PushpaSowjanya@Pushpa-iMac.local>
2019-01-31 10:23:38 +0530

    Removed login and saved as Login page

---
+++ src/components/app/loginpage.js
@@ -0,0 +1,75 @@
+import React, { Component } from "react";
+import crime1 from "./images/crime1.jpg";
+
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
+
+const logincss={
+Align:'center',
+marginTop:'250px'
+    
+}
+const imgcontainer={
+    textAlign: 'center',
+    margin: '24px 0 12px 0'
+  }
+  const avatar= {
+    width: '300px',
+    Height: '300px',
+    borderRadius: '360px'
+  }
+const inputstyle={
+    width:'400px'
+}
+
+class Loginpage extends Component {
+  render() {
+    return (
+      <div style={logincss} >
+      <center>
+      <Form className="form">
+          <fieldset>
+        <legend><h2>LOGIN</h2></legend>
+       
+        <div style={imgcontainer}>
+             <img src={crime1} alt="Avatar" style={avatar}></img>
+               </div>
+          <Col>
+            <FormGroup>
+              <Label>Enter Email</Label>
+              <Input
+                type="email"
+                name="email"
+                id="exampleEmail"
+                placeholder="mygmail@gmail.com"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label for="examplePassword">Enter Password</Label>
+              <Input
+                type="password"
+                name="password"
+                id="examplePassword"
+                placeholder="********"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <a href="#">Not a registered user Sign in</a>
+          <br />
+          <Button>Login</Button>
+          </fieldset>
+        </Form>
+        </center>
+      </div>
+    );
+  }
+}
+
+export default Loginpage;
