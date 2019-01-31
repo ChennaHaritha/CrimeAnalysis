1985404afb67276ea736953aa04005d1e826a7af
Chinni Pushpa Sowjanya <PushpaSowjanya@Pushpa-iMac.local>
2019-01-31 10:35:29 +0530

    Modified register page

--- src/components/app/registerCrimes.js
+++ src/components/app/registerCrimes.js
@@ -1,9 +1,171 @@
-import React from 'react';
-import { Button, Form, FormGroup, Label, Input, FormText } from 'reactstrap';
+import React, { Component } from "react";
+import crime1 from "./images/crime1.jpg";
 
-class RegisterCrime extends React.Component {
+import {
+  Container, Col, Form,
+  FormGroup, Label, Input,
+  Button,
+} from 'reactstrap';
+
+const logincss={
+Align:'center',
+marginTop:'150px'
+    
+}
+const imgcontainer={
+    textAlign: 'center',
+    margin: '24px 0 12px 0'
+  }
+  const avatar= {
+    width: '300px',
+    Height: '300px',
+    borderRadius: '360px'
+  }
+const inputstyle={
+    width:'400px'
+}
+
+class Sign extends Component {
   render() {
     return (
+<<<<<<< HEAD
+      <div style={logincss} >
+      <center>
+      <Form className="form">
+          <fieldset>
+        <legend><h2>RegisterCrimes</h2></legend>
+       
+        <div style={imgcontainer}>
+             <img src={crime1} alt="Avatar" style={avatar}></img>
+               </div>
+               <Col>
+            <FormGroup>
+              <Label>CrimeType</Label>
+              <Input
+                type="name"
+                name="name"
+                id="examplename"
+                placeholder="CrimeType"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Year</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleid"
+                placeholder="enter year"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Gender</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleGender"
+                placeholder="Gender"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Age</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleAge"
+                placeholder="Age"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>City</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleCity"
+                placeholder="City"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>No.of People Affected</Label>
+              <Input
+                type="name"
+                name="name"
+                id="examplePopulation"
+                placeholder="PeopleAffected"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>TimeOfDay</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleDay"
+                placeholder="TimefDay"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Lat,Lon</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleLat"
+                placeholder="Enter Lat,Lon"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Description</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleDescription"
+                placeholder="Enter Description"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          <Col>
+            <FormGroup>
+              <Label>Suspects</Label>
+              <Input
+                type="name"
+                name="name"
+                id="exampleSuspects"
+                placeholder="Suspects"
+                style={inputstyle}
+              />
+            </FormGroup>
+          </Col>
+          
+          <br />
+          <Button>Submit</Button>
+          </fieldset>
+        </Form>
+        </center>
+      </div>
+=======
       <Form>
         <FormGroup>
           <Label for="CrimeType">CrimeType</Label>
@@ -74,8 +236,9 @@ class RegisterCrime extends React.Component {
         </FormGroup>
         <Button>Submit</Button>
       </Form>
+>>>>>>> 727c765af9d53a703af6bbd2bb73adcedee6cfc1
     );
   }
 }
 
-export default RegisterCrime;
+export default Sign;
